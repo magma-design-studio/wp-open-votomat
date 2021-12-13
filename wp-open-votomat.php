31d75b05db6eec0340c03223ac8f44320c50c485
Sebastian Tiede <tiede@magmadesignstudio.de>
2021-12-13 10:20:46 +0100

    Commit for version 1.0.8

--- wp-open-votomat.php
+++ wp-open-votomat.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Open Votomat
  * Description: This plugin allows you to use your website as a voting advice application.
  * Author: magma design studio
- * Version: 1.0.5
+ * Version: 1.0.8
  * Author URI: https://magmadesignstudio.de
  * Text Domain: wpov
  * License:     GPL2
@@ -27,7 +27,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 define( 'WPOV__PLUGIN_NAME_SLUG', 'wp-open-votomat' );
 
-define( 'WPOV_VERSION', '1.0.5' );
+define( 'WPOV_VERSION', '1.0.8' );
 define( 'WPOV__MINIMUM_WP_VERSION', '5.8.2' );
 define( 'WPOV__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WPOV__PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
